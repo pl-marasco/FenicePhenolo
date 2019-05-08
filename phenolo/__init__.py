92be9a84ed433a3c4f199820738dbdfa1d53a650
pl.marasco@gmail.com <pl.marasco@gmail.com>
2019-05-08 15:34:17 +0200

    first commit

---
+++ phenolo/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+Phenolo
+
+Post fata resurgo
+
+'''
+
+from .atoms import *
+from .chronos import *
+from .filters import *
+from .metrics import *
+from .nodata import *
+from .outlier import *
+from .output import *
+from .peaks import *
+from .reader import *
+from .settings import *
