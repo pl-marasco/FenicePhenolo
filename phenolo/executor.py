9a5b92624c7b23002fda6b9f3d13e309b7f9c185
pl.marasco@gmail.com <pl.marasco@gmail.com>
2019-09-03 17:55:55 +0200

    dictionary was expecting str instead got int

--- phenolo/executor.py
+++ phenolo/executor.py
@@ -117,10 +117,10 @@ def _filler(key, pxldrl, att, col):
 
 
 def _error_decoder(err):
-    err_cod = {'1': 'No data', '2': 'Scaling', '3': 'Off set', '4': '0-100 Scaling', '5': 'outlier filtering',
-               '6': 'gap filling', '7': 'Season and trend estimation', '8': 'To daily conversion', '9': 'madspan error',
-               '10': 'Trend conversion to daily', '11': 'Savinsky Golet', '12': 'Valley detection',
-               '13': 'Season detection', '14': 'Season mean', '15': 'Season metrics'}
+    err_cod = {1: 'No data', 2: 'Scaling', 3: 'Off set', 4: '0-100 Scaling', 5: 'outlier filtering',
+               6: 'gap filling', 7: 'Season and trend estimation', 8: 'To daily conversion', 9: 'madspan error',
+               10: 'Trend conversion to daily', 11: 'Savinsky Golet', 12: 'Valley detection',
+               13: 'Season detection', 14: 'Season mean', 15: 'Season metrics'}
 
     return err_cod[err]
 
