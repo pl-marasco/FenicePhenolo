029fb81e6f332ca84a9ed48ea7f51aed3f63cc97
pl.marasco@gmail.com <pl.marasco@gmail.com>
2019-11-11 08:10:35 +0100

    time delta to 0.25 and posix time divided through / and not //

--- phenolo/atoms.py
+++ phenolo/atoms.py
@@ -96,7 +96,7 @@ class SingularCycle(object):
         self.sd = sd  # Start date - MBD
         self.ed = ed  # End date - MED
         self.mml = self.__time_delta(self.sd, self.ed)  # Cycle length in days
-        self.td = self.mml * 1 / 10  # time delta
+        self.td = self.mml * 0.25  # time delta
         self.mms_b = ts.loc[sd - self.td:ed + self.td]  # buffered time series #TODO verify possible referencing
         self.mms = self.mms_b.loc[sd:ed]  # minimum minimum time series
         self.stb = self.__integral(self.mms)  # Standing biomass (minimum minimum integral) [mi] [VOX x cycle]
@@ -207,7 +207,7 @@ class SingularCycle(object):
         """Barycenter"""
         cbc = 0
         try:
-            self.posix_time = self.vox.index.astype(np.int64) // 10 ** 9
+            self.posix_time = self.vox.index.astype(np.int64) / 10 ** 9
             cbc = (self.posix_time * self.vox).sum() / self.vox_i
         except(RuntimeError, Exception, ValueError):
             self.err = True
